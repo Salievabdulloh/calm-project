d56a8bb6df419cadf5fb603a1f98657b29608e72
Saliev_Abdulloh_07 <jonsonme771@gmail.com>
2025-03-26 14:43:33 +0500

    just started

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      "tel": "375px",
+      "tab": "758px",
+      "lap": "1440px",
+    } 
+  },
+  plugins: [],
+}
